1c3f83ddc3ab05110e6b32d2eb5d8498d7e35cb8
changhong.wang <wangchanghong@codemao.cn>
2021-06-30 06:26:47 +0800

    feat: redux

--- server/src/containers/Home/index.js
+++ server/src/containers/Home/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 // 同构：一套React代码，在服务器端执行一次，再客户端再执行一次
 
-const Home = () => {
+const Home = (props) => {
 	return (
 		<div>
-			<div>This is Dell Lee!</div>
+			<div>This is {props.name}!</div>
 			<button onClick={()=>{alert('click1')}}>
 				click
 			</button>
@@ -13,4 +14,8 @@ const Home = () => {
 	)
 }
 
-export default Home;
+const mapStateToProps = state => ({
+	name: state.name
+})
+
+export default connect(mapStateToProps, null)(Home);
