728ac0d044a2ca5327482f3529339540ee0edd37
changhong <changhongwang@126.com>
2021-06-30 06:41:46 +0800

    feat: store合并; prettier

--- server/src/server/utils.js
+++ server/src/server/utils.js
@@ -1,27 +1,20 @@
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { StaticRouter } from 'react-router-dom';
-import Routes from '../Routes';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
+import React from "react";
+import { renderToString } from "react-dom/server";
+import { StaticRouter } from "react-router-dom";
+import Routes from "../Routes";
+import { Provider } from "react-redux";
+import getStore from "../store";
 
 export const render = (req) => {
-	const reducer = (state = { name: 'dell' }, action) => {
-		return state;
-	}
-	
-	const store = createStore(reducer, applyMiddleware(thunk));
+  const content = renderToString(
+    <Provider store={getStore()}>
+      <StaticRouter location={req.path} context={{}}>
+        {Routes}
+      </StaticRouter>
+    </Provider>
+  );
 
-	const content = renderToString((
-		<Provider store={store}>
-			<StaticRouter location={req.path} context={{}}>
-				{Routes}
-			</StaticRouter>
-		</Provider>
-	));
-
-	return `
+  return `
 		<html>
 			<head>
 				<title>ssr</title>
@@ -32,4 +25,4 @@ export const render = (req) => {
 			</body>
 		</html>
   `;
-}
+};
