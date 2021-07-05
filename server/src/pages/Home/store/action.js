75132783386cd65172b00722e26daa3c6f0b2f2c
changhong <changhongwang@126.com>
2021-07-05 19:11:53 +0800

    opti: 避免传入server参数; 用withExtraArgument进行优化

--- server/src/pages/Home/store/action.js
+++ server/src/pages/Home/store/action.js
@@ -1,5 +1,5 @@
 // 负责所有action
-import { getNewsList } from "../../../api";
+import { SECRET_KEY } from "../../../common/const";
 import { ACTION_TYPE } from "./const";
 
 // action 不应该写成一个对象，而是应该由actionCreator来创建
@@ -8,15 +8,15 @@ const changeList = (list) => ({
   list,
 });
 
-export const getHomeList = (server) => {
-  return (dispatch) => {
-    return getNewsList(server)
-      .then((res) => {
-        dispatch(changeList(res.data));
-      })
-      .catch((e) => {
-        console.log(e);
+export const getHomeList = () => {
+  return (dispatch, getState, axios) => {
+    return axios.get(`/api/news.json?secret=${SECRET_KEY}`).then((res) => {
+      if (res.status === 200) {
+        dispatch(changeList(res.data.data));
+      } else {
         dispatch(changeList([]));
-      });
+        reject(`异常， 状态码: ${res.status}`);
+      }
+    });
   };
 };
--- server/src/store/index.js
+++ server/src/store/index.js
@@ -1,16 +1,26 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import { reducer as homeReducer } from "../pages/Home/store";
+import clinetAxios from "../client/request";
+import serverAxios from "../server/request";
 
 const reducer = combineReducers({
   home: homeReducer,
 });
 
+// 通过withExtraArgument传入axios 避免通过传入server形参区分调用不同的axios
 export const getStore = () => {
-  return createStore(reducer, applyMiddleware(thunk));
+  return createStore(
+    reducer,
+    applyMiddleware(thunk.withExtraArgument(serverAxios))
+  );
 };
 
 export const getClientStore = () => {
   const defaultState = window.context.state;
-  return createStore(reducer, defaultState, applyMiddleware(thunk));
+  return createStore(
+    reducer,
+    defaultState,
+    applyMiddleware(thunk.withExtraArgument(clinetAxios))
+  );
 };
