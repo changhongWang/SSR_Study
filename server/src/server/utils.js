5462a1469747bf42960308a09548ddc6f4c76df2
changhong.wang <wangchanghong@codemao.cn>
2021-07-05 19:58:49 +0800

    feat: renderRoutes处理多级路由

--- server/src/server/utils.js
+++ server/src/server/utils.js
@@ -1,20 +1,15 @@
 import React from "react";
 import { renderToString } from "react-dom/server";
 import { StaticRouter, Route } from "react-router-dom";
-import { matchRoutes } from "react-router-config";
+import { renderRoutes } from "react-router-config";
 import { Provider } from "react-redux";
-import { getStore } from "../store";
 
 export const render = (store, routes, req) => {
   // 拿到异步数据，填充到store里，填充的数据根据path来
   const content = renderToString(
     <Provider store={store}>
       <StaticRouter location={req.path} context={{}}>
-        <div>
-          {routes.map((route) => (
-            <Route {...route} key={route.key} />
-          ))}
-        </div>
+        <div>{renderRoutes(routes)}</div>
       </StaticRouter>
     </Provider>
   );
