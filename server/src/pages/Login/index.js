cf7c412a600630180d493335738de20a44c64fd1
changhong <changhongwang@126.com>
2021-07-08 07:33:39 +0800

    feat: 登录功能实现

--- server/src/pages/Login/index.js
+++ server/src/pages/Login/index.js
@@ -1,12 +1,57 @@
 import React from "react";
-import Header from "../../components/Header";
+import { connect } from "react-redux";
+import { actions } from "../../store/user";
 
-const Login = () => {
+const renderLoggedIn = (props) => {
   return (
     <div>
-      <div>Login</div>
+      <div>Welcome! Wang</div>
+      <button
+        onClick={() => {
+          props.logout();
+        }}
+      >
+        点此登出
+      </button>
     </div>
   );
 };
 
-export default Login;
+const renderNotLogIn = (props) => {
+  return (
+    <div>
+      <div>未登录</div>
+      <button
+        onClick={() => {
+          props.login();
+        }}
+      >
+        点此登录
+      </button>
+    </div>
+  );
+};
+const Login = (props) => {
+  return (
+    <div>{props.isLogin ? renderLoggedIn(props) : renderNotLogIn(props)}</div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    isLogin: state.user.isLogin,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  login: () => {
+    console.log("page login");
+    dispatch(actions.login());
+  },
+  logout: () => {
+    console.log("page log out");
+    dispatch(actions.logout());
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
---
+++ server/src/store/user/const.js
@@ -0,0 +1,3 @@
+export const ACTION_TYPE = {
+  CHANGE_LOGIN_STAT: "USER/CHANGE_LOGIN_STAT",
+};
